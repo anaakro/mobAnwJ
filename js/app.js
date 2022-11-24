9b9c83482dfbaeba533758a35a48defe54c170ca
JoelHerold <86368518+JoelHerold@users.noreply.github.com>
2022-11-24 17:52:20 +0100

    test_1.12

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
+const container = document.querySelector(".container");
 
+if ("serviceWorker" in navigator) { //check if the Service Worker supported in the current Browser
+    window.addEventListener("load", function() { // listen to the page load event to register our service worker
+        navigator.serviceWorker
+            .register("/serviceWorker.js")
+            .then(res => console.log("service worker registered"))
+            .catch(err => console.log("service worker not registered", err));
+    });
+}
