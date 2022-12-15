eb10ff0ca12b15e187513f55d2d907d0203b6541
JoelHerold <86368518+JoelHerold@users.noreply.github.com>
2022-12-15 15:28:45 +0100

    test_1.17

--- js/inputCosts.js
+++ js/inputCosts.js
@@ -4,8 +4,11 @@ console.log("test")
 var table = document.getElementById("myTable");
 
 function test(){
-    var testValue = document.getElementById("description").value;
-    var inputForm = document.getElementById("form1").innerHTML
+    var in1 = document.getElementById("description").value;
+    var in2 = document.getElementById("date").value;
+    var in3 = document.getElementById("category").value;
+    var in4 = document.getElementById("amount").value;
+    var in5 = document.getElementById("balance").value;
     console.log(testValue)
     console.log(inputForm)
     console.log("hallo")
