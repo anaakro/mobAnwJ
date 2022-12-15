38e8d1ca210e3d518e4c0b02bf1e00d01fa9fb72
JoelHerold <86368518+JoelHerold@users.noreply.github.com>
2022-12-15 15:14:25 +0100

    test_1.17

---
+++ js/inputCosts.js
@@ -0,0 +1,12 @@
+
+
+
+var table = document.getElementById("myTable");
+
+function test(){
+    var testValue = document.getElementById("description");
+    var inputForm = document.getElementById("form1")
+    console.log(testValue)
+    console.log(inputForm)
+
+}
