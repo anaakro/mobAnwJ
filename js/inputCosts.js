b05e5f2c7e1e3f22b11bb3be86036a4089e22d6a
JoelHerold <86368518+JoelHerold@users.noreply.github.com>
2022-12-15 15:55:40 +0100

    test_1.17

--- js/inputCosts.js
+++ js/inputCosts.js
@@ -3,13 +3,16 @@ console.log("test")
 
 var table = document.getElementById("myTable");
 
+
+var JsonData = {};
+
 function test(){
     var in1 = document.getElementById("description").value;
     var in2 = document.getElementById("date").value;
     var in3 = document.getElementById("category").value;
     var in4 = document.getElementById("amount").value;
     var in5 = document.getElementById("balance").value;
-    console.log(testValue)
+    console.log(in1)
     console.log(inputForm)
     console.log("hallo")
 
