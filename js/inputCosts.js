099e054a45ccbf32fe4e46032101fd677b4fc64e
JoelHerold <86368518+JoelHerold@users.noreply.github.com>
2022-12-15 16:08:35 +0100

    test_1.17

--- js/inputCosts.js
+++ js/inputCosts.js
@@ -6,6 +6,10 @@ var table = document.getElementById("myTable");
 
 var JsonData = {};
 var list1 = [];
+var list2 = [];
+var list3 = [];
+var list4 = [];
+var list5 = [];
 
 function test(){
     var in1 = document.getElementById("description").value;
@@ -15,10 +19,18 @@ function test(){
     var in5 = document.getElementById("balance").value;
     console.log(in1)
     list1.push(in1)
+    list1.push(in2)
+    list1.push(in3)
+    list1.push(in4)
+    list1.push(in5)
     //console.log(inputForm)
     console.log("hallo")
 
     localStorage.setItem('description', list1);
+    localStorage.setItem('date', list2);
+    localStorage.setItem('category', list3);
+    localStorage.setItem('amount', list4);
+    localStorage.setItem('balance', list5);
 
 }
 
